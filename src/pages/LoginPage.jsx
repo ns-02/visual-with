d952fea0724b21d51324d5709e042b360898428a
ns-02 <lemen267@gmail.com>
2025-10-14 13:23:54 +0900

    버튼 클릭 및 유효성 검사

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -8,17 +8,32 @@ function LoginPage() {
         <form>
           <div>
             <div>
-              <input placeholder="ID를 입력하세요"></input>
+              <input id="useremail" type="text" placeholder="이메일을 입력하세요" required></input>
             </div>
             <div>
-              <input placeholder="비밀번호를 입력하세요"></input>
+              <input id="userpassword" type="password" placeholder="비밀번호를 입력하세요" required></input>
             </div>
             {/* type="button"으로 일부로 전송을 막아놓은 것! 필요시 해제 */}
             <div>
-              <button type="button">로그인</button> 
+              <button type="button" onClick={() => {
+                let useremail = document.getElementById("useremail").value;
+                let userpassword = document.getElementById("userpassword").value;
+
+                if (!useremail || !userpassword) {
+                  alert("양식이 입력되지 않았습니다.");
+                  console.log("양식이 입력되지 않았습니다.");
+                  return;
+                }
+
+                console.log(`유저 이메일: ${useremail}`);
+                console.log(`유저 페스워드: ${userpassword}`);
+                console.log("로그인");
+              }}>로그인</button> 
             </div>
             <div>
-              <button type="button">회원가입</button> 
+              <button type="button" onClick={() => {
+                console.log("회원가입");
+              }}>회원가입</button> 
             </div>
           </div>
           
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -8,20 +8,42 @@ function SignupPage() {
         <form>
           <div>
             <div>
-              <p>ID</p>
-              <input></input>
+              <p>이메일</p>
+              <input id="useremail" type="text"></input>
             </div>
             <div>
               <p>비밀번호</p>
-              <input></input>
+              <input id="userpassword" type="password"></input>
             </div>
             <div>
               <p>비밀번호 확인</p>
-              <input></input>
+              <input id="checkuserpassword" type="password"></input>
             </div>
             {/* type="button"으로 일부로 전송을 막아놓은 것! 필요시 해제 */}
             <div>
-              <button type="button">회원가입</button> 
+              <button type="button" onClick={() => {
+                let useremail = document.getElementById("useremail").value;
+                let userpassword = document.getElementById("userpassword").value;
+                let checkuserpassword = document.getElementById("checkuserpassword").value;
+
+                if (!useremail || !userpassword || !checkuserpassword) {
+                  alert("양식이 입력되지 않았습니다.");
+                  console.log("양식이 입력되지 않았습니다.");
+                  return;
+                }
+
+                if (userpassword !== checkuserpassword) {
+                  alert("비밀번호가 일치하지 않습니다.");
+                  console.log("비밀번호가 일치하지 않습니다.");
+                  return;
+                }
+
+                console.log(`유저 이메일: ${useremail}`);
+                console.log(`유저 페스워드: ${userpassword}`);
+                console.log("회원가입");
+
+                // 회원가입 완료를 알리는 페이지를 따로 만들어야하나?
+              }}>회원가입</button> 
             </div>
           </div>
           
