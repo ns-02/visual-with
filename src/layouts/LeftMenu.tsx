7033d4609c86ad7cf501b767bdf4ed6ac97237e4
ns-02 <lemen267@gmail.com>
2025-10-18 07:22:03 +0900

    메뉴 스위칭

--- src/layouts/LeftMenu.tsx
+++ src/layouts/LeftMenu.tsx
@@ -1,21 +1,55 @@
+import { useState } from 'react';
 import './layouts.css'
 
 function LeftMenu() {
+  const [ isViewTool, setIsViewTool ] = useState(true);
+  const [ isViewTeam, setIsViewTeam ] = useState(false);
+
   return (
     <>
       <section className='leftmenu'>
         <div>
-          <button>버튼 1</button>
+          <button onClick={() => {
+            setIsViewTool(!isViewTool);
+            setIsViewTeam(!isViewTeam);
+          }}>토글 버튼</button>
         </div>
-        <div>
-          <button>버튼 2</button>
-        </div>
-        <div>
-          <button>버튼 3</button>
-        </div>
-        <div>
-          <button>버튼 4</button>
-        </div>
+        <br></br>
+        {
+          isViewTool && 
+          <div className='tool'>
+            <div>
+              <button>도구 1</button>
+            </div>
+            <div>
+              <button>도구 2</button>
+            </div>
+            <div>
+              <button>도구 3</button>
+            </div>
+            <div>
+              <button>도구 4</button>
+            </div>
+          </div>
+        }
+        {
+          isViewTeam && 
+          <div className='team'>
+            <div>
+              <button>팀 1</button>
+            </div>
+            <div>
+              <button>팀 2</button>
+            </div>
+            <div>
+              <button>팀 3</button>
+            </div>
+            <div>
+              <button>팀 4</button>
+            </div>
+          </div>
+        }
+        
       </section>
     </>
   )
