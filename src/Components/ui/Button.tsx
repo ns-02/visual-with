99093d5ab0f5d3d9c4dc97dc61337672c7014ab3
sg02 <sg3m02@gmail.com>
2025-10-26 11:38:26 +0900

    아이콘 포함 버튼

--- src/Components/ui/Button.tsx
+++ src/Components/ui/Button.tsx
@@ -1,15 +1,16 @@
+import { Plus } from 'lucide-react';
 import "./Button.css"
 
 interface ButtonProps {
   text: string;
   onClick: () => void;
-  icon?: React.ReactNode;
+  icon?: typeof Plus;
 }
 
-const Button: React.FC<ButtonProps> = ({ text, onClick, icon }) => {
+const Button: React.FC<ButtonProps> = ({ text, onClick, icon: Icon }) => {
   return (
     <button className="c-button" onClick={onClick}>
-      {icon}
+      {Icon && <Icon className="c-icon" />}
       {text}
     </button>
   )
--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import Button from "../components/ui/Button";
 import { fn } from 'storybook/internal/test';
+import { Plus } from 'lucide-react';
 
 const meta = {
   title: 'Example/Button',
@@ -16,5 +17,13 @@ export const Primary: Story = {
   args: {
     text: '버튼',
     onClick: fn(),
+  },
+};
+
+export const Icon: Story = {
+  args: {
+    text: '버튼',
+    onClick: fn(),
+    icon: Plus,
   },
 };
