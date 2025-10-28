475e217ddf2b6881d14286b0233b305d97110db7
sg02 <sg3m02@gmail.com>
2025-10-28 09:40:03 +0900

    세션스토리지 적용, 마지막 채팅 미적용 문제 해결

--- src/features/teamChat/pages/ChatPage.tsx
+++ src/features/teamChat/pages/ChatPage.tsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 import ChatContents from "../section/ChatContents";
 import ChatBottom from "../section/ChatBottom";
 import './ChatPage.css'
+import { getItem, setItem } from "../../../utils/sessionStorage";
 
 function ChatPage() {
-  const [ allChat, setAllChat ] = useState("");
+  const initialState = getItem('teamChats', "");
+
+  const [ allChat, setAllChat ] = useState(initialState);
   const [ chat, setChat ] = useState("");
 
+  // 채팅을 입력할 때마다 ChatPage()가 계속 재랜더링됨
+
   const onSend = () => {
-    setAllChat(`${allChat}${chat}\n`);
-    setChat("");
+    let today = new Date();
+    let time = (today.toLocaleTimeString() + "");
+
+    let nextAllChat = `${allChat}${chat}\n [ ${time.slice(0, time.length - 3)} ]\n`;
+
+    setItem('teamChats', JSON.stringify(nextAllChat));
+    setAllChat(nextAllChat);
+    setChat("");    
   };
 
   return (
