28f1d3f73ff304de7ba21543e76d66f3e1b3db1f
ns-02 <sg3m02@gmail.com>
2025-11-12 10:39:37 +0900

    스타일 및 표시 형식 조정

--- src/main/features/schedule/section/LeftCalendar.tsx
+++ src/main/features/schedule/section/LeftCalendar.tsx
@@ -1,11 +1,22 @@
+import { useState } from 'react';
 import Calender from '../components/Calendar';
 import styles from './ScheduleSection.module.css'
 
 function LeftCalendar() {
+  const [selected, setSelected] = useState<Date>();
+  let today = new Date().getDate();
+
   return (
     <div className={styles["left-calender"]}>
       <div className={styles.calender_container}>
-        <Calender />
+        <Calender selected={selected} setSelected={setSelected} />
+      </div>
+      <br />
+      <div>
+        <p>{selected ? `${selected.getDate()}일` : `${today}일 (오늘)`}</p>
+        <p>일정 제목 1</p>
+        <p>일정 제목 2</p>
+        <p>일정 제목 3</p>
       </div>
     </div>
   )
--- src/main/features/schedule/section/RightContents.tsx
+++ src/main/features/schedule/section/RightContents.tsx
@@ -1,10 +1,13 @@
 import styles from './ScheduleSection.module.css'
 
 function RightContents() {
+  let month = new Date().getMonth() + 1;
+  let today = new Date().getDate();
+
   return (
     <div className={styles["right-contents"]}>
       <div>
-        오늘 (XX월 XX일)
+        {`오늘 (${month}월 ${today}일)`}
       </div>
       <div>
         예정된 일정
