ffccf7125eabdbbd7046ae6edb8346254cea9815
ns-02 <sg3m02@gmail.com>
2025-11-18 08:11:59 +0900

    채팅 요소 스타일 적용

--- src/components/ChatView.tsx
+++ src/components/ChatView.tsx
@@ -17,9 +17,11 @@ const ChatView = React.memo(({ allChat }: Props) => {
         {
           allChat.map((chatItem, index) => {
             return (
-              <div key={index}> {/* 이거 가능한 한 빨리 수정해야 함.. */}
-                <p>{chatItem.chat}</p>
-                <p>[ {chatItem.time} ]</p>
+              <div key={index} > {/* 이거 가능한 한 빨리 수정해야 함.. */}
+                <p style={{ fontSize: "13px", color: "#777" }}>{chatItem.time}</p>
+                <div style={{ display: "inline-block", padding: "8px 12px", marginBottom: "8px", borderRadius: "10px",  backgroundColor: "white" }}>
+                  <p style={{ display: "inline-block" }} >{chatItem.chat}</p>
+                </div>
               </div>
             )
           })
