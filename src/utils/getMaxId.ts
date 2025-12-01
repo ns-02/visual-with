d7576d0ffc06ddd08bb01324823293d4dc130ed5
ns-02 <sg3m02@gmail.com>
2025-12-02 08:12:20 +0900

    오류 수정

--- src/utils/getMaxId.ts
+++ src/utils/getMaxId.ts
@@ -1,8 +1,4 @@
 const getMaxId = (items: any[]) => {
-  if (!items[0].id) {
-    console.error("Error: Object for getMaxId must contain 'id'.");
-  }
-
   return items.length > 0 ? Math.max(...items.map((item) => item.id)) : 0;
 };
 
