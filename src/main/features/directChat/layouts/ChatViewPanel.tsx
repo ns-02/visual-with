ed9cf73a3d469cb217e8324c04f73dfb55916de8
ns-02 <sg3m02@gmail.com>
2025-12-11 22:38:39 +0900

    친구 미선택 시 조건부 출력

--- src/main/features/directChat/layouts/ChatViewPanel.tsx
+++ src/main/features/directChat/layouts/ChatViewPanel.tsx
@@ -1,7 +1,18 @@
 import { Prop } from '..';
 import styles from './DirectChatLayout.module.css';
+import { useFriend } from '@context/FriendContext';
 
 function ChatViewPanel({ children }: Prop) {
+  const { selectFriendData } = useFriend();
+
+  if (!selectFriendData) {
+    return (
+      <div className={styles.chat_view_panel}>
+        <div className={styles.overview}>준비 중인 화면입니다</div>
+      </div>
+    );
+  }
+
   return <div className={styles.chat_view_panel}>{children}</div>;
 }
 
