a51a3c9d7a3780c3dfda084e059c53374af2cc7a
ns-02 <sg3m02@gmail.com>
2026-06-02 10:37:41 +0900

    eslint 설정 변경

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,7 @@ export default defineConfig(
     },
     rules: {
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
     settings: {
       react: {
