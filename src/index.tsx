bb27f4482de7b56ba639aa7a7a4f43e7fe7c2afa
ns-02 <sg3m02@gmail.com>
2026-06-05 13:45:18 +0900

    mock 서버 끄기

--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,29 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-async function enableMocking() {
-  const { worker } = await import('./mocks/browser');
-  return worker.start({
-    onUnhandledRequest: 'bypass',
-  });
-}
+// async function enableMocking() {
+//   const { worker } = await import('./mocks/browser');
+//   return worker.start({
+//     onUnhandledRequest: 'bypass',
+//   });
+// }
 
-enableMocking().then(() => {
-  const root = createRoot(document.getElementById('root') as HTMLElement);
-  root.render(
-    <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </StrictMode>,
-  );
-});
+// enableMocking().then(() => {
+//   const root = createRoot(document.getElementById('root') as HTMLElement);
+//   root.render(
+//     <StrictMode>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </StrictMode>,
+//   );
+// });
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
