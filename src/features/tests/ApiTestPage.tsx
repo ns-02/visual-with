36fded1486abdf33047782c0d3284e9f566bc326
ns-02 <sg3m02@gmail.com>
2026-06-12 11:32:28 +0900

    테스트 페이지 수정

--- src/features/tests/ApiTestPage.tsx
+++ src/features/tests/ApiTestPage.tsx
@@ -1,23 +1,39 @@
-import { Button } from '@shared/components';
+import { useUserStore } from '@core/store/useUserStore';
+import { useWorkspaceStore } from '@core/store/useWorkspaceStore';
+import { selectTeamList, selectTeamMemberList } from '@shared/api/baseApi';
+import { Button, Input } from '@shared/components';
 import { useState } from 'react';
-
-// 예시 함수
-function testFn() {
-  return {
-    value: 'Hello World!',
-  };
-}
 
 export function ApiTestPage() {
   const [result, setResult] = useState<null | unknown>(null);
+  const defaultUserId = useUserStore((state) => state.user)?.id ?? '';
+  const defaultTeamId = useWorkspaceStore((state) => state.selectTeamId) ?? '';
+
+  const [userId, setUserId] = useState(defaultUserId);
+  const [teamId, setTeamId] = useState(defaultTeamId);
 
   const handleClick = async () => {
-    const data = await testFn();
+    if (!teamId) return;
+
+    const data = await selectTeamMemberList({ teamId });
     setResult(data);
   };
 
   return (
     <>
+      <Input
+        value={userId}
+        setChat={setUserId}
+        placeholder='userId'
+        sizeMode='flexible'
+      />
+      <Input
+        value={teamId}
+        setChat={setTeamId}
+        placeholder='teamId'
+        sizeMode='flexible'
+      />
+
       <Button style={{ backgroundColor: 'aliceblue' }} onClick={handleClick}>
         API 호출
       </Button>
