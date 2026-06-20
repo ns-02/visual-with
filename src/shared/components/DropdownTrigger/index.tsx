5eadd8d18d8b0315b7b42487b0f129f023aeea9f
ns-02 <sg3m02@gmail.com>
2026-06-20 12:57:24 +0900

    드롭다운이 표시되지 않는 문제 수정
    - DropdownMenuTrigger에 forwardRef 적용
    - 컴포넌트 래핑 과정에서 Radix UI ref 전달이 끊어져 드롭다운이 열리지 않던 문제 해결
    
    배운 점:
    - Radix UI의 asChild는 단순히 UI만 교체하는 것이 아니라 ref와 이벤트를 자식에게 전달함
    - asChild 대상 컴포넌트는 forwardRef를 지원해야 정상 동작함

--- src/shared/components/DropdownTrigger/index.tsx
+++ src/shared/components/DropdownTrigger/index.tsx
@@ -1,12 +1,15 @@
+import { forwardRef } from 'react';
 import { EllipsisVertical } from 'lucide-react';
 import Button from '../Button';
 
-const DropdownTrigger = () => {
+const DropdownTrigger = forwardRef<HTMLButtonElement>((props, ref) => {
   return (
-    <Button variant='content'>
+    <Button variant='content' ref={ref} {...props}>
       <EllipsisVertical size={16} />
     </Button>
   );
-};
+});
+
+DropdownTrigger.displayName = 'DropdownTrigger';
 
 export default DropdownTrigger;
