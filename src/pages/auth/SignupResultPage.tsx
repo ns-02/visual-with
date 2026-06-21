2b88234d7b2e147d2a4933b8126cc4657e1617ff
ns-02 <sg3m02@gmail.com>
2026-06-21 16:01:06 +0900

    회원가입 완료 페이지 수정

--- src/pages/auth/SignupResultPage.tsx
+++ src/pages/auth/SignupResultPage.tsx
@@ -26,7 +26,10 @@ function SignupResultPage() {
         <h1 className={styles.title}>회원가입 완료</h1>
       </div>
       <p>{`${userId}님의 회원가입을 환영합니다!`}</p>
-      <Button onClick={() => navigate('/login')} variant='auth'>
+      <Button
+        onClick={() => navigate('/login', { replace: true })}
+        variant='auth'
+      >
         로그인
       </Button>
     </div>
